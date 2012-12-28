0f3c62bd8e195ebbc34f4093d8e01f22caab8441
smstern <sascha.morgenstern@gmail.com>
2012-12-28 08:58:11 +0100

    feature: introduce basic authentication

--- G3PublishService.lrplugin/Info.lua
+++ G3PublishService.lrplugin/Info.lua
@@ -16,7 +16,7 @@ return {
 	
 	LrPluginInfoProvider = 'LRInfoProvider.lua',
 	
-	VERSION = { major=0, minor=3, revision=6, build=0, },
+	VERSION = { major=0, minor=4, revision=0, build=0, },
 	
 	LrInitPlugin = 'LRInitPlugin.lua'
 
